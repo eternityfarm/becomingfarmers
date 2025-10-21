16a737271687047da66e4281e2068d33f61f34b1
eternityfarm <sungminb87@gmail.com>
2025-10-21 16:21:51 +0900

    Create Mission.js

---
+++ src/pages/About/Mission.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function Mission(){
+return (
+<div className="card p-6">
+<h2 className="text-2xl font-bold mb-2">미션과 철학</h2>
+<p className="text-gray-500">추후 업로드 예정입니다.</p>
+</div>
+);
+}
