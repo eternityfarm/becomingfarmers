18ea554ebf876da5482507450343476be876987c
eternityfarm <sungminb87@gmail.com>
2025-10-21 18:00:42 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { HashRouter } from "react-router-dom"; // Vercel 환경에서도 흰 화면 없이 안정적
 
-
-// Pages
-import Home from "./pages/Home";
-import Overview from "./pages/About/Overview";
-import Mission from "./pages/About/Mission";
-import RegionSelect from "./pages/Market/RegionSelect";
-import CategorySelect from "./pages/Market/CategorySelect";
-import ProductList from "./pages/Market/ProductList";
-import ProductDetail from "./pages/Market/ProductDetail";
-import RegisterForm from "./pages/Register/RegisterForm";
-import Login from "./pages/Auth/Login";
-import Signup from "./pages/Auth/Signup";
-
-
-export default function App() {
-return (
-<div className="min-h-screen flex flex-col bg-gradient-to-br from-white to-green-50">
-<Navbar />
-<main className="flex-1 max-w-6xl w-full mx-auto px-4 py-6">
-<Routes>
-<Route path="/" element={<Home />} />
+// React 앱을 root 엘리먼트에 렌더링
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
 
 
 {/* About */}
